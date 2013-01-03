fc97725d007f8d557b94be2a60a6071b780d27ab
andrew_hipp (Andrew Hipp) <ahipp@mortonarb.org>
2013-01-03 15:44:19 +0000

    updating to utilize Biostrings where possible... faster and better
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/morton@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/r2pyRAD.R
+++ pkg/R/r2pyRAD.R
@@ -52,17 +52,20 @@ con.pyRAD.multicore <- function(x, cores = 4) {
   return(out)
   }
 
-consensus.pyRAD <- function(pyIn, from = NA, to = NA, fastaNames = T, writeFile = 'rads.con.1_100.txt', method = 'majority', threshold = 0.001) {
+consensus.pyRAD <- function(pyIn, from = NA, to = NA, fastaNames = T, writeFile = 'rads.con.1_100.txt', ...) {
 ## use seqinr to generate a consensus sequence for each pyRAD locus
+## 2013-01-04: updated to use Biostrings, which works better -- deleted arguments: method = 'majority', threshold = 0.001
   if(class(pyIn) != "pyRAD.loci") stop("pyRAD input required, from read.pyRAD")
-  require(seqinr)
+  # require(seqinr)
+  require(Biostrings)
   allLoci <- unique(as.character(pyIn$locus.index))
   allLoci <- allLoci[!allLoci == ""] ## this should probably part of read.pyRAD
   if(!is.na(from)) allLoci <- allLoci[from:to]
   seqs <- as.character(pyIn$seqs)
   loc.index <- as.character(pyIn$locus.index)
   out <- character(0)
-  for(i in allLoci) out <- c(out, paste(consensus(str2mat(seqs[loc.index == i]), method, threshold), collapse = ""))
+  for (i in allLoci) out <- c(out, consensusString(DNAStringSet(seqs[loc.index == i]), ...))
+  # for(i in allLoci) out <- c(out, paste(consensus(str2mat(seqs[loc.index == i]), method, threshold), collapse = ""))
   if(fastaNames) allLoci <- paste(">", allLoci, sep = "")
   names(out) <- allLoci
   if(!is.na(writeFile)) write.table(out, writeFile, sep = "\n", quote = F, col.names = F)
