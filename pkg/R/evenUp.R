88b2a1a2c8f7218b908398a862fd3863cf90f2ad
andrew_hipp (Andrew Hipp) <ahipp@mortonarb.org>
2013-01-23 15:11:14 +0000

    allowing evenUp to take an absolute depth
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/morton@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/evenUp.R
+++ pkg/R/evenUp.R
@@ -2,12 +2,13 @@
 ## useful if you have manually added tips and need to clean things up a bit
 ## Andrew Hipp, 20 Nov 2012
 
-evenUp.phylo <- function(tr, method = c('median', 'mean', 'min', 'max')) {
+evenUp.phylo <- function(tr, absDepth = NA, method = c('median', 'mean', 'min', 'max')) {
   require(phytools)
   if(class(tr) != "phylo") stop('phylo object required')
   tipLabels <- which(tr$edge[, 2] <= length(tr$tip.label))
   tipHeights <- nodeHeights(tr)[tipLabels, 2]
-  tipsOverage <- tipHeights - switch(method[1], 
+  if(!is.na(absDepth)) tipsOverage <- tipHeights - absDepth
+  else tipsOverage <- tipHeights - switch(method[1], 
                                  median = median(tipHeights),
 								 mean = mean(tipHeights),
 					             min = min(tipHeights),
