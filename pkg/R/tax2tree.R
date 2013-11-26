7f72d1045728fc33dfc8552fb904e55ea446818e
andrew_hipp (Andrew Hipp) <ahipp@mortonarb.org>
2013-11-26 11:47:07 +0000

    adding tax2tree.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/morton@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/tax2tree.R
@@ -0,0 +1,30 @@
+## make a tree from the trelease hierarchy, p. 20
+## ahipp@mortonarb.org, 2013-11-25
+
+trelease.table <- read.delim('trelease.taxonomy.2013-11-25.trial', as.is = T)
+
+tree.from.taxonomy <- function(taxonomy = trelease.table) {
+  nEdge = dim(taxonomy)[1]
+  ntips <- sum(taxonomy$tip)
+  tr <- list(edge = matrix(NA, nrow = nEdge, ncol = 2),
+			 edge.length = rep(1, nEdge), 
+			 tip.label = taxonomy$terminus[taxonomy$tip]
+			 )
+  tr$edge[taxonomy$tip, 2] <- seq(ntips)
+  tr$edge[!taxonomy$tip, 2] <- (ntips + 2):(nEdge + 1)
+  tr$edge[, 1] <- tr$edge[match(taxonomy$parent, taxonomy$terminus), 2]
+  tr$edge[is.na(tr$edge[, 1]), 1] <- ntips + 1
+  tr$node.label = taxonomy$parent[match((ntips + 1):(nEdge + 1), tr$edge[, 1])]
+  tr$Nnode <- 1 + (nEdge - ntips)
+  class(tr) <- 'phylo'
+  return(tr)
+  }
+
+dist.nodes.2 <- function(x, drop.pattern = 'n[0123456789]') {
+  require(ape)
+  out <- dist.nodes(x)
+  out.names <- c(x$tip.label, x$node.label)
+  dimnames(out) <- list(out.names, out.names)
+  out <- out[-grep(drop.pattern, out.names), -grep(drop.pattern, out.names)]
+  return(out)
+  }
